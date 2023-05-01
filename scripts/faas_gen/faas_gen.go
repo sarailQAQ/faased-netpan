2d017b8900cd8fbb4ed46c9d962aac33a35928ba
sarailqaq <847117505@qq.com>
2023-05-01 19:41:51 +0800

    Update

--- scripts/faas_gen/faas_gen.go
+++ scripts/faas_gen/faas_gen.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -19,19 +20,70 @@ func main() {
 	if !strings.HasSuffix(*tarDir, "/") {
 		*tarDir = *tarDir + "/"
 	}
-	funcNames, _ := filepath.Glob(*srcDir + "*.go")
-	for _, funcPath := range funcNames {
+
+	// 创建 handlers 文件夹
+	err := exec.Command("mkdir", *tarDir+"handlers").Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 根据文件名生成创建对应的 openfaas 函数
+	funcPaths, _ := filepath.Glob(*srcDir + "*.go")
+	var funcNames []string
+	for i, funcPath := range funcPaths {
 		split := strings.Split(funcPath, "/")
 		funcFileName := split[len(split)-1]
 		funcName := substr(funcFileName, 0, len(funcFileName)-3)
-		fmt.Println(funcName)
-		cmd := exec.Command("faas new", "--lang", "golang-middleware", "--handler", *tarDir+"handlers", funcName)
-		fmt.Println(cmd.String())
+		fmt.Println(i, funcName)
+		funcName = strings.ReplaceAll(funcName, "_", "-")
+		cmd := exec.Command("faas", "new", "--lang", "golang-middleware", "--handler", *tarDir+"handlers/"+funcName+"-handler", funcName)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("combined out:\n%s\n", string(out))
+			log.Fatalf("cmd.Run() failed with %s\n", err)
+		}
+		funcNames = append(funcNames, funcName)
 	}
 
+	for _, funcName := range funcNames {
+		path := funcName + ".yml"
+		cmd := exec.Command("mv", path, *tarDir+path)
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	//for _, funcPath := range funcPaths {
+	//	newFuncPath := fileRename(funcPath)
+	//	fmt.Println(funcPath, newFuncPath)
+	//	if funcPath == newFuncPath {
+	//		continue
+	//	}
+	//
+	//	cmd := exec.Command("mv", funcPath, newFuncPath)
+	//	err := cmd.Run()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//}
 }
 
 func substr(str string, idx, length int) string {
 	split := strings.Split(str, "")
 	return strings.Join(split[idx:idx+length], "")
+}
+
+func fileRename(s string) (new string) {
+	new = ""
+	for i, r := range s {
+		if 'A' <= r && r <= 'Z' {
+			if i != 0 {
+				new += "_"
+			}
+			r += 'a' - 'A'
+		}
+		new += string(r)
+	}
+	return new
 }
--- third_part/openfaas/hello/go.mod
+++
@@ -1,8 +0,0 @@
-module handler/function
-
-go 1.18
-
-require (
-	github.com/openfaas/templates-sdk/go-http v0.0.0-20220408082716-5981c545cb03
-	github.com/sarailQAQ/faased-netpan v0.0.0-20230413091434-24878cd706f1
-)
